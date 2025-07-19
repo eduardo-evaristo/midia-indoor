7e1ef7d8d2ae20602639548b30f2039ffee7036a
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-07-18 22:19:51 -0300

    refactor(auth): update credentials to use email and enhance authorization logic

--- auth.ts
+++ auth.ts
@@ -3,20 +3,28 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { prisma } from "./prisma";
 import Credentials from "next-auth/providers/credentials";
+import { getUserByEmail } from "@/lib/db/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub,
     Credentials({
       credentials: {
-        username: { label: "Username" },
+        email: { label: "Email" },
         password: { label: "Password", type: "password" },
       },
-      authorize: (credentials, request) => {
-        console.log(credentials, request);
-        return null;
+      authorize: async (credentials, request) => {
+        console.log("do we get here?");
+        const user = await getUserByEmail(credentials.email as string);
+        console.log(user);
+        if (!user) return null;
+        return user;
       },
     }),
   ],
   adapter: PrismaAdapter(prisma),
+  pages: { signIn: "/auth/login" },
+  session: {
+    strategy: "jwt",
+  },
 });
